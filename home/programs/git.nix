cd06204c7cc4335d0bea76725cd4d75bac4bc587
mcanueste <mcanueste@gmail.com>
2022-07-17 20:35:50 +0200

    add git aliases

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -6,6 +6,16 @@
       lfs.enable = true;
       userName = "mcanueste";
       userEmail = "mcanueste@gmail.com";
+      aliases = {
+        a = "add";
+        s = "status";
+        p = "push";
+	pl = "pull";
+        b = "branch";
+        c = "commit";
+        cm = "commit -m";
+        co = "checkout";
+      };
     };
   };
 }
