5f2650e3b1bc1d7dd5b791c34ebfd1661cd5d5dc
mcanueste <mcanueste@gmail.com>
2022-07-22 15:48:35 +0200

    update starship configs

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, ... }:
 {
   home-manager.users.mcst = {
     programs.starship = {
@@ -6,6 +6,56 @@
       enableBashIntegration = true;
       enableZshIntegration = false;
       enableFishIntegration = false;
+      settings = {
+        add_newline = false;
+        scan_timeout = 10;
+        format = lib.concatStrings [
+          "$username"
+          "$hostname"
+          "$localip"
+          "$shlvl"
+          "$kubernetes"
+          "$directory"
+          "$vcsh"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_metrics"
+          "$git_status"
+          "$hg_branch"
+          "$docker_context"
+          "$package"
+          "$buf"
+          "$c"
+          "$cmake"
+          "$container"
+          "$golang"
+          "$haskell"
+          "$helm"
+          "$lua"
+          "$python"
+          "$rust"
+          "$terraform"
+          "$vagrant"
+          "$nix_shell"
+          "$memory_usage"
+          "$aws"
+          "$gcloud"
+          "$openstack"
+          "$azure"
+          "$env_var"
+          "$custom"
+          "$sudo"
+          "$cmd_duration"
+          "$line_break"
+          "$jobs"
+          "$battery"
+          "$time"
+          "$status"
+          "$shell"
+          "$character"
+        ];
+      };
     };
   };
 }
