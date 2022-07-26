7872391309deb371bfdbff5ea2d448f0d8fa5031
mcanueste <mcanueste@gmail.com>
2022-07-26 23:09:47 +0200

    update home manager version to unstable

--- home/home.nix
+++ home/home.nix
@@ -1,8 +1,7 @@
 { pkgs, ... }:
 
 let
-  # home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
 in
 {
   imports = [ 
@@ -24,18 +23,18 @@ in
 
   # Enable lorri service
   # services.lorri.enable = true;
-  
+
   # User based nixpkgs config
   home-manager.users.mcst = {
+    home.stateVersion = "22.05";
+
     xdg = {
       enable = true;
       configFile."nixpkgs/config.nix".source = ./nixpkgs-config.nix;
     };
-  };
 
-  home-manager.users.mcst = {
     home.packages = [
       (pkgs.nerdfonts.override { fonts = [ "SourceCodePro" "Hack" ]; })
     ];
-  }; 
+  };
 }
