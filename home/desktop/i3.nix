9185c4c427ff7e6c720061289252fd8a7c950e22
mcanueste <mcanueste@gmail.com>
2023-06-26 22:41:06 +0200

    remove unnecessary blocks from i3 bar

--- home/desktop/i3.nix
+++ home/desktop/i3.nix
@@ -330,31 +330,6 @@ in {
           };
         };
         blocks = [
-          {
-            block = "load";
-            interval = 1;
-            format = " $icon  $1m.eng(w:4) ";
-          }
-          {
-            block = "cpu";
-            interval = 1;
-          }
-          {
-            block = "memory";
-            format = " $icon $mem_used_percents.eng(w:1) ";
-            format_alt = " $icon_swap $swap_free.eng(w:3,u:B,p:M)/$swap_total.eng(w:3,u:B,p:M)($swap_used_percents.eng(w:2)) ";
-            interval = 10;
-            warning_mem = 70;
-            critical_mem = 90;
-          }
-          {
-            block = "disk_space";
-            path = "/";
-            info_type = "available";
-            interval = 60;
-            warning = 20.0;
-            alert = 10.0;
-          }
           {block = "sound";}
           {
             block = "backlight";
