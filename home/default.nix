c68759edacefdf2ba94a23c76c58464877d35267
mcanueste <mcanueste@gmail.com>
2023-06-26 23:00:33 +0200

    update nixos version strings to 23.11

--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,5 @@
 {
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.11";
   programs.home-manager.enable = true;
 
   imports = [
--- os/default.nix
+++ os/default.nix
@@ -10,7 +10,7 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11";
+  system.stateVersion = "23.11"; # not considered since we use unstable via flake, but anyways
   nixpkgs.hostPlatform = "x86_64-linux";
   nixpkgs.config.allowUnfree = true;
   nix.settings = {
