af158993491d70dff2695f83e483da40114df94e
mcanueste <mcanueste@gmail.com>
2023-07-10 21:21:34 +0200

    add vim-just plugin

--- home/editors/neovim.nix
+++ home/editors/neovim.nix
@@ -22,6 +22,16 @@ with pkgs.lib.conflib; let
     };
   };
 
+  vim-just = pkgs.vimUtils.buildVimPlugin {
+    name = "vim-just";
+    src = pkgs.fetchFromGitHub {
+      owner = "NoahTheDuke";
+      repo = "vim-just";
+      rev = "10de9ebf0bd8df8ff8593b0b87ec8bf3b715326f";
+      sha256 = "sha256-NGhWF4/SEPww9e/wCDghGMSPZmmAbms6tn/IHqDMDkI=";
+    };
+  };
+
   shellAliases = {
     v = "nvim";
   };
@@ -156,7 +166,7 @@ in {
           # }))
           # vimPlugins.copilot-lua
         ]
-        ++ [nvim-config capture-nvim];
+        ++ [nvim-config capture-nvim vim-just];
 
       extraConfig = ''
         lua << EOF
