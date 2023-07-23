baa19c99c8f90e11534a0f6f752dbc79a4046328
mcanueste <mcanueste@gmail.com>
2023-07-23 12:22:44 +0200

    add gimp to installed packages

--- home/editors/other.nix
+++ home/editors/other.nix
@@ -8,6 +8,7 @@ with pkgs.lib.conflib; let
 in {
   options.nixhome.editors = {
     obsidian = mkBoolOption {description = "Enable Obsidian";};
+    gimp = mkBoolOption {description = "Enable gimp";};
     datagrip = mkBoolOption {
       description = "Enable JetBrains Datagrip";
       default = false;
@@ -21,6 +22,7 @@ in {
   config = {
     home.packages = filterPackages [
       (getPackageIf cfg.obsidian pkgs.obsidian)
+      (getPackageIf cfg.gimp pkgs.gimp)
       (getPackageIf cfg.datagrip pkgs.jetbrains.datagrip)
       (getPackageIf cfg.pycharm pkgs.jetbrains.pycharm-professional)
     ];
