b81e30dbd483373642b2904e8d4386b4ec83b1d9
mcanueste <mcanueste@gmail.com>
2024-03-14 19:00:16 +0100

    update ai configs for neovim

--- modules/editors/nvim/lua/config/ai.lua
+++ modules/editors/nvim/lua/config/ai.lua
@@ -4,9 +4,9 @@ local chatgpt = require("chatgpt")
 
 local function init()
     local home = vim.fn.expand("$HOME")
-    whichkey.register({ a = { name = "ai" } }, { prefix = "<leader>" })
+    whichkey.register({ a = { name = "ai" }, ac = { name = "copilot" } }, { prefix = "<leader>" })
 
-    require("copilot").setup({
+    copilot.setup({
         panel = {
             enabled = true,
             auto_refresh = false,
@@ -14,8 +14,8 @@ local function init()
                 jump_prev = "[[",
                 jump_next = "]]",
                 accept = "<CR>",
-                refresh = "gr",
-                open = "<M-CR>",
+                refresh = "<C-r>",
+                open = "<C-CR>",
             },
             layout = {
                 position = "bottom", -- | top | left | right
@@ -25,10 +25,10 @@ local function init()
         suggestion = {
             auto_trigger = true,
             keymap = {
-                accept = "<M-l>",
-                dismiss = "<M-h>",
-                next = "<M-j>",
-                prev = "<M-k>",
+                accept = "<C-l>",
+                dismiss = "<C-h>",
+                next = "<C-j>",
+                prev = "<C-k>",
                 accept_word = false,
                 accept_line = false,
             },
@@ -46,45 +46,87 @@ local function init()
         },
     })
 
+    vim.keymap.set("n", "<leader>act", function()
+        require("copilot.suggestion").toggle_auto_trigger()
+    end, { noremap = true, desc = "Copilot Toggle Auto Trigger" })
+
+    vim.keymap.set("n", "<leader>acp", "<cmd>Copilot panel<cr>", { noremap = true, desc = "Copilot Panel" })
+
     chatgpt.setup({
-        api_key_cmd = "cat " .. home .. "/.ssh/chatgpt.key",
+        api_key_cmd = "cat " .. home .. "/.ssh/openai.key",
+        predefined_chat_gpt_prompts = "https://raw.githubusercontent.com/f/awesome-chatgpt-prompts/main/prompts.csv",
         openai_params = {
-            model = "gpt-3.5-turbo",
+            model = "gpt-4",
+            frequency_penalty = 0,
+            presence_penalty = 0,
             max_tokens = 300,
+            temperature = 0,
+            top_p = 1,
+            n = 1,
         },
         openai_edit_params = {
             -- model = "gpt-3.5-turbo",
-            model = "gpt-4", -- NOTE: don't have access now for some weird reason
+            model = "gpt-4",
+            frequency_penalty = 0,
+            presence_penalty = 0,
+            temperature = 0,
+            top_p = 1,
+            n = 1,
+        },
+        edit_with_instructions = {
+            diff = false,
+            keymaps = {
+                close = "<C-c>",
+                accept = "<C-y>",
+                toggle_diff = "<C-d>",
+                toggle_settings = "<C-o>",
+                toggle_help = "<C-h>",
+                cycle_windows = "<Tab>",
+                use_output_as_input = "<C-i>",
+            },
+        },
+        chat = {
+            sessions_window = {
+                active_sign = " 󰄵 ",
+                inactive_sign = " 󰄱 ",
+            },
+            keymaps = {
+                close = "<C-c>",
+                yank_last = "<C-y>",
+                yank_last_code = "<C-k>",
+                scroll_up = "<C-u>",
+                scroll_down = "<C-d>",
+                new_session = "<C-n>",
+                cycle_windows = "<Tab>",
+                cycle_modes = "<C-f>",
+                next_message = "<C-j>",
+                prev_message = "<C-k>",
+                select_session = "<Space>",
+                rename_session = "r",
+                delete_session = "d",
+                draft_message = "<C-r>",
+                edit_message = "e",
+                delete_message = "d",
+                toggle_settings = "<C-o>",
+                toggle_sessions = "<C-p>",
+                toggle_help = "<C-h>",
+                toggle_message_role = "<C-r>",
+                toggle_system_role_open = "<C-s>",
+                stop_generating = "<C-x>",
+            },
         },
     })
 
-    -- FIXME: buggy UI with this
-    vim.keymap.set("n", "<leader>ac", function()
-        chatgpt.complete_code()
-    end, { noremap = true, desc = "ChatGPT Complete" })
-
-    vim.keymap.set("n", "<leader>ae", function()
-        chatgpt.edit_with_instructions()
-    end, { noremap = true, desc = "ChatGPT Edit" })
+    -- GPT Prompts
+    vim.keymap.set("n", "<leader>ap", function()
+        chatgpt.openChat()
+    end, { noremap = true, desc = "ChatGPT Prompt" })
 
     vim.keymap.set("n", "<leader>aa", function()
         chatgpt.selectAwesomePrompt()
     end, { noremap = true, desc = "ChatGPT Act as" })
 
-    vim.keymap.set("n", "<leader>ap", function()
-        chatgpt.openChat()
-    end, { noremap = true, desc = "ChatGPT Prompt" })
-
-    vim.keymap.set("n", "<leader>at", "<cmd>ChatGPTRun add_tests<cr>", { noremap = true, desc = "ChatGPT Add Test" })
-    vim.keymap.set("n", "<leader>af", "<cmd>ChatGPTRun fix_bugs<cr>", { noremap = true, desc = "ChatGPT Fix Bugs" })
-    -- FIXME: works a bit weird
-    vim.keymap.set("n", "<leader>ad", "<cmd>ChatGPTRun docstring<cr>", { noremap = true, desc = "ChatGPT Docstring" })
-    vim.keymap.set(
-        "n",
-        "<leader>ae",
-        "<cmd>ChatGPTRun explain_code<cr>",
-        { noremap = true, desc = "ChatGPT Explain Code" }
-    )
+    -- Code related commands
     vim.keymap.set(
         "n",
         "<leader>ao",
@@ -97,8 +139,20 @@ local function init()
         "<cmd>ChatGPTRun code_readability_analysis<cr>",
         { noremap = true, desc = "ChatGPT Code Readability Analysis" }
     )
+    vim.keymap.set("n", "<leader>ae", function()
+        chatgpt.edit_with_instructions()
+    end, { noremap = true, desc = "ChatGPT Edit" })
+    vim.keymap.set("n", "<leader>ad", "<cmd>ChatGPTRun docstring<cr>", { noremap = true, desc = "ChatGPT Docstring" })
+    vim.keymap.set("n", "<leader>at", "<cmd>ChatGPTRun add_tests<cr>", { noremap = true, desc = "ChatGPT Add Test" })
+    vim.keymap.set("n", "<leader>af", "<cmd>ChatGPTRun fix_bugs<cr>", { noremap = true, desc = "ChatGPT Fix Bugs" })
+    vim.keymap.set(
+        "n",
+        "<leader>aE",
+        "<cmd>ChatGPTRun explain_code<cr>",
+        { noremap = true, desc = "ChatGPT Explain Code" }
+    )
 
-    -- TODO: optimize these later when needed
+    -- Text editing related commands
     vim.keymap.set("n", "<leader>as", "<cmd>ChatGPTRun summarize<cr>", { noremap = true, desc = "ChatGPT Summarize" })
     vim.keymap.set("n", "<leader>an", "<cmd>ChatGPTRun translate<cr>", { noremap = true, desc = "ChatGPT Translate" })
     vim.keymap.set("n", "<leader>ak", "<cmd>ChatGPTRun keywords<cr>", { noremap = true, desc = "ChatGPT Keywords" })
