e019d2dc3877b7a86abe1c9a66059944dfc01f09
mcanueste <mcanueste@gmail.com>
2024-04-02 18:33:36 +0200

    update flake.lock and add gcloud plugins

--- modules/tools/cloud.nix
+++ modules/tools/cloud.nix
@@ -40,10 +40,16 @@
     home-manager.users.${config.nixconf.user} = {
       home.packages = lib.lists.flatten [
         (lib.lists.optional config.nixconf.tools.aws pkgs.awscli2)
-        (lib.lists.optional config.nixconf.tools.gcloud pkgs.google-cloud-sdk)
         (lib.lists.optional config.nixconf.tools.azure pkgs.azure-cli)
         (lib.lists.optional config.nixconf.tools.digital-ocean pkgs.doctl)
         (lib.lists.optional config.nixconf.tools.cfssl pkgs.cfssl)
+        (lib.lists.optional config.nixconf.tools.gcloud (pkgs.google-cloud-sdk.withExtraComponents [
+          pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
+          # pkgs.google-cloud-sdk.components.kubectl
+          pkgs.google-cloud-sdk.components.kubectl-oidc
+          pkgs.google-cloud-sdk.components.terraform-tools
+          pkgs.google-cloud-sdk.components.docker-credential-gcr
+        ]))
       ];
     };
   };
