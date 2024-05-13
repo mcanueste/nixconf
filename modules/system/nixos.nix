75fd258ff21da4a5171083ce35731d9f0c72afbf
mcanueste <mcanueste@gmail.com>
2024-05-13 14:47:45 +0200

    use latest version of nix cli instead of git version

--- modules/system/nixos.nix
+++ modules/system/nixos.nix
@@ -8,7 +8,7 @@
   nixpkgs.config.allowUnfree = true;
 
   # Flakes settings
-  nix.package = pkgs.nixVersions.git;
+  nix.package = pkgs.nixVersions.latest;
   nix.registry.nixpkgs.flake = inputs.nixpkgs;
   nix.settings.experimental-features = ["nix-command" "flakes"];
   nix.settings.warn-dirty = false;
