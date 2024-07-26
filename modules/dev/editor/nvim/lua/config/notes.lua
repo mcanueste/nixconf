989cfdb2382a0230690904d2dc77c5ea8f6d7056
mcanueste <mcanueste@gmail.com>
2024-07-26 14:06:12 +0200

    update obsidian.nvim configs

--- modules/dev/editor/nvim/lua/config/notes.lua
+++ modules/dev/editor/nvim/lua/config/notes.lua
@@ -1,5 +1,30 @@
 local obsidian = require("obsidian")
 
+local date = function()
+    return os.date("%Y-%m-%d")
+end
+
+local date_verbose = function()
+    return os.date("%A, %B %d, %Y")
+end
+
+local daily = function()
+    return os.date("logs/%Y/%m/%Y-%m-%d")
+end
+
+local daily_previous = function()
+    return os.date("logs/%Y/%m/%Y-%m-%d", os.time() - 24 * 60 * 60)
+end
+
+local daily_next = function()
+    return os.date("logs/%Y/%m/%Y-%m-%d", os.time() + 24 * 60 * 60)
+end
+
+local weekly = function()
+    -- Week starts from Monday
+    return os.date("logs/%Y/Weekly/%yW%V", os.time() - 24 * 60 * 60)
+end
+
 local function init()
     -- Obsidian.nvim setup
     -- See: https://github.com/epwalsh/obsidian.nvim
@@ -7,6 +32,7 @@ local function init()
 
     local home = vim.fn.expand("$HOME")
     local vault_path = home .. "/Projects/personal/notes"
+
     ---@diagnostic disable-next-line: missing-fields
     local client = obsidian.setup({
         dir = vault_path,
@@ -24,17 +50,22 @@ local function init()
             date_format = "%Y-%m-%d",
             time_format = "%H:%M",
             -- A map for custom variables, the key should be the variable and the value a function
-            substitutions = {},
+            substitutions = {
+                -- date = date,
+                date_verbose = date_verbose,
+                daily = daily,
+                daily_previous = daily_previous,
+                daily_next = daily_next,
+                weekly = weekly,
+            },
         },
 
-        attachments = {
-            img_folder = "_attachments",
-        },
+        attachments = { img_folder = "_attachments" },
 
         daily_notes = {
             folder = "logs",
-            date_format = "%Y-%m-%d",
-            template = "basic.md",
+            date_format = "%Y/%m/%Y-%m-%d",
+            template = "daily.md",
         },
 
         note_id_func = function(title)
@@ -77,10 +108,22 @@ local function init()
         },
     })
 
-    vim.keymap.set("n", "<leader>no", "<cmd>ObsidianOpen<cr>", { noremap = true, desc = "Open" })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        desc = "Update lastmod metadata on save",
+        pattern = "*.md",
+        group = vim.api.nvim_create_augroup("config_notes", { clear = true }),
+        command = "%s/^lastmod: 20.*/lastmod: " .. os.date("%Y-%m-%d") .. "/ | normal! ``<cr>",
+    })
+
+    vim.keymap.set("n", "<leader>nO", "<cmd>ObsidianOpen<cr>", { noremap = true, desc = "Open" })
     vim.keymap.set("n", "<leader>nn", "<cmd>ObsidianNew<cr>", { noremap = true, desc = "New Note" })
+    vim.keymap.set("n", "<leader>nN", "<cmd>ObsidianNewFromTemplate<cr>", { noremap = true, desc = "New Note (T)" })
     vim.keymap.set("n", "<leader>nf", "<cmd>ObsidianQuickSwitch<cr>", { noremap = true, desc = "Find" })
-    vim.keymap.set("n", "<leader>nF", "<cmd>ObsidianSearch<cr>", { noremap = true, desc = "Find or Create" })
+    vim.keymap.set("n", "<leader>ng", "<cmd>ObsidianSearch<cr>", { noremap = true, desc = "Grep or Create" })
+    vim.keymap.set("n", "<leader>nG", "<cmd>ObsidianTags<cr>", { noremap = true, desc = "Tag Search" })
+    vim.keymap.set("n", "<leader>nt", "<cmd>ObsidianToday<cr>", { noremap = true, desc = "Today" })
+    vim.keymap.set("n", "<leader>ny", "<cmd>ObsidianYesterday<cr>", { noremap = true, desc = "Yesterday" })
+    vim.keymap.set("n", "<leader>nm", "<cmd>ObsidianTemplate<cr>", { noremap = true, desc = "Template" })
     vim.keymap.set(
         "n",
         "<leader>nv",
@@ -93,14 +136,11 @@ local function init()
         "<cmd>ObsidianFollowLink hsplit<cr>",
         { noremap = true, desc = "Follow Horizontal" }
     )
-    vim.keymap.set("n", "<leader>nt", "<cmd>ObsidianToday<cr>", { noremap = true, desc = "Today" })
-    vim.keymap.set("n", "<leader>ny", "<cmd>ObsidianYesterday<cr>", { noremap = true, desc = "Yesterday" })
-    vim.keymap.set("n", "<leader>ng", "<cmd>ObsidianTags<cr>", { noremap = true, desc = "Tag Search" })
-    vim.keymap.set("n", "<leader>nT", "<cmd>ObsidianTemplate<cr>", { noremap = true, desc = "Template" })
-    vim.keymap.set("v", "<leader>nl", "<cmd>ObsidianLink<cr>", { noremap = true, desc = "Link to File" })
-    vim.keymap.set("v", "<leader>nL", "<cmd>ObsidianLinkNew<cr>", { noremap = true, desc = "Link to New File" })
     vim.keymap.set("n", "<leader>nl", "<cmd>ObsidianLinks<cr>", { noremap = true, desc = "Links" })
     vim.keymap.set("n", "<leader>nb", "<cmd>ObsidianBacklinks<cr>", { noremap = true, desc = "Backlinks" })
+    vim.keymap.set("n", "<leader>nT", "<cmd>ObsidianTOC<cr>", { noremap = true, desc = "TOC" })
+    vim.keymap.set("v", "<leader>nl", "<cmd>ObsidianLink<cr>", { noremap = true, desc = "Link to File" })
+    vim.keymap.set("v", "<leader>nL", "<cmd>ObsidianLinkNew<cr>", { noremap = true, desc = "Link to New File" })
     vim.keymap.set("v", "<leader>ne", "<cmd>ObsidianExtractNote<cr>", { noremap = true, desc = "Extract Note" })
     vim.keymap.set("n", "<leader>ni", "<cmd>ObsidianPasteImg<cr>", { noremap = true, desc = "Paste Image" })
     vim.keymap.set(
