7d9518ba41d9d60d1f52849998bc3b62f58ad3fc
mcanueste <mcanueste@gmail.com>
2024-10-04 09:52:53 +0200

    disable rustaceanvim plugin

--- nixos/editor/neovim.nix
+++ nixos/editor/neovim.nix
@@ -90,7 +90,7 @@ in {
             nvim-lspconfig
             none-ls-nvim
             neodev-nvim
-            rustaceanvim
+            # rustaceanvim
             nvim-dap
             nvim-nio
             nvim-dap-ui
