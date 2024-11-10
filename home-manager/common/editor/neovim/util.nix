7f86199bf07432e634ace27664bbfe66d02b3b5e
mcanueste <mcanueste@gmail.com>
2024-11-10 09:38:05 +0100

    replace vim-sleuth with guess-indent.nvim

--- home-manager/common/editor/neovim/util.nix
+++ home-manager/common/editor/neovim/util.nix
@@ -1,7 +1,7 @@
 {
   programs.nixvim.plugins = {
     tmux-navigator.enable = true;
-    sleuth.enable = true;
+    guess-indent.enable = true;
     nvim-autopairs = {
       enable = true;
       settings.disable_filetype = ["TelescopePrompt"];
